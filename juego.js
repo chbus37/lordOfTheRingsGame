7d63f97654d08e932c94e148ff4612851b1c05ca
Christian Bustos <christianbus37@gmail.com>
2024-01-28 18:22:35 -0300

    juego terminado

--- juego.js
+++ juego.js
@@ -4,8 +4,45 @@ function Jugador(nombre){
     this.nombre = nombre;
     this.pv = 100;
     this.sp = 100;
+
+    this.curar = function(jugadorObjetivo){
+
+        if(this.sp >= 40 ){
+            this.sp -= 40;
+            jugadorObjetivo.pv = jugadorObjetivo.pv + 20;
+        }else{
+            console.info(`${this.nombre} no tiene SP`);
+        }
+
+        this.estado(jugadorObjetivo);
+ 
+    }
+
+    this.tirarFlecha = function(jugadorObjetivo){
+
+        if(jugadorObjetivo.pv > 40){
+            jugadorObjetivo.pv -= 40;
+        }else{
+            jugadorObjetivo.pv = 0;
+            console.error(jugadorObjetivo.nombre + ' murio!');
+        }
+        
+        this.estado(jugadorObjetivo);
+
+    }
+
+    this.estado = function(jugadorObjetivo){
+        console.log(this);
+        console.log(jugadorObjetivo);
+    }
 }
 
 
-let gandalf = new Jugador();
-let legolas = new Jugador();
+let gandalf = new Jugador('Gandalf');
+let legolas = new Jugador('Legolas');
+
+console.log(gandalf);
+console.log(legolas);
+
+
+
